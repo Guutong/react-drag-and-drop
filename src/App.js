7cee561de758396ab3b7f016f188d2425540a4cd
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-06-30 23:45:20 +0700

    add dragable

--- src/App.js
+++ src/App.js
@@ -1,42 +1,90 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
 
-export class App extends React.Component {
   state = {
     items: [
       {
         id: 1,
-        order: 1,
         name: "item 1"
       },
       {
         id: 2,
-        order: 2,
         name: "item 2"
       },
       {
         id: 3,
-        order: 3,
         name: "item 3"
       },
       {
         id: 4,
-        order: 4,
         name: "item 4"
       }
     ]
   };
+
+  onDragEnd(result) {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="card">
-          {this.state.items.length > 0 ? (
-            this.state.items.map((item, index) => <div className="item">{item.name}</div>)
-          ) : (
-            <div className="item">no row</div>
-          )}
-        </div>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div
+                className="card"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {this.state.items &&
+                  this.state.items.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          className="item"
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          {item.name}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     );
   }
