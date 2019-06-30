e4c96f855ae8829771a46488b7b9e4ce8da355e9
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-06-30 23:20:20 +0700

    add list ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        order: 1,
+        name: "item 1"
+      },
+      {
+        id: 2,
+        order: 2,
+        name: "item 2"
+      },
+      {
+        id: 3,
+        order: 3,
+        name: "item 3"
+      },
+      {
+        id: 4,
+        order: 4,
+        name: "item 4"
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="card">
+          {this.state.items.length > 0 ? (
+            this.state.items.map((item, index) => <div className="item">{item.name}</div>)
+          ) : (
+            <div className="item">no row</div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
