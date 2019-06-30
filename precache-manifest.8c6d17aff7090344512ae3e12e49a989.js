ecb23ffb1e0bc53503f168f2a837e33de73f9b1c
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-07-01 00:55:29 +0700

    Updates

---
+++ precache-manifest.8c6d17aff7090344512ae3e12e49a989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a8c9e1eb8e73d928cbe530cd4c9b12",
+    "url": "/react-drag-and-drop/index.html"
+  },
+  {
+    "revision": "9447bdb64dc6d7b8090f",
+    "url": "/react-drag-and-drop/static/css/main.492a51ec.chunk.css"
+  },
+  {
+    "revision": "191d803f5f390bcc2367",
+    "url": "/react-drag-and-drop/static/js/2.cec6bdb8.chunk.js"
+  },
+  {
+    "revision": "9447bdb64dc6d7b8090f",
+    "url": "/react-drag-and-drop/static/js/main.8919e2a8.chunk.js"
+  },
+  {
+    "revision": "04298665d7f1ef9daff5",
+    "url": "/react-drag-and-drop/static/js/runtime~main.edbf4f1c.js"
+  }
+]);
